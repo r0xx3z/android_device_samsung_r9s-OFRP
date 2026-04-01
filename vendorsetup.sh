45295c29b1495d322e0a1e3b4cd863ec3d7bdffb
Llorens <axelgpg2290@gmail.com>
2026-04-01 04:02:06 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,33 +1,66 @@
-FDEVICE="r9s"
-#set -0 xtrace
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="marble"
+#set -o xtrace
 
 fox_get_target_device() {
-local chdev=$(echo "BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chdev" ]; then 
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi 
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
 }
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then 
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
    fox_get_target_device
 fi
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then 
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
+ #version & variant
+export FOX_VERSION="R12.0"
+export FOX_VARIANT="A15"
+export FOX_BUILD_TYPE="Stable"
 
-
-
-
-
-  lunch twrp_$FDEVICE-eng
-  #let's see what are our build VARs
-  if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then 
-       export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-       export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-       export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-       export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-  fi
+ #OFOX Addons
+export FOX_ENABLE_APP_MANAGER=1
+#OFR binary files
+export FOX_REPLACE_BUSYBOX_PS=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_REPLACE_TOOLBOX_GETPROP=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_NANO_EDITOR=1
+	
+	lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
 fi
+
+
