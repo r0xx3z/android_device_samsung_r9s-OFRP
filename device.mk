ee1766b46c3b43c278aa12a5000012080bfc3790
Llorens <axelgpg2290@gmail.com>
2026-04-02 00:29:11 -0500

    Clean up device.mk by removing unnecessary lines
    Removed unused variables and cleaned up the device.mk file.

--- device.mk
+++ device.mk
@@ -36,7 +36,6 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
-TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
@@ -51,10 +50,7 @@ TW_INCLUDE_FASTBOOTD := true
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := r9s
-
 TARGET_COPY_OUT_VENDOR := vendor
-
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
