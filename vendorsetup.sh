75d6dcb62aac3a802179596b684b453f50764dab
Llorens <axelgpg2290@gmail.com>
2026-05-02 20:42:41 -0500

    avoid redundancy
    Updated settings in vendorsetup.sh.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,10 +22,12 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
 export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.7.zip
 export TW_DEFAULT_LANGUAGE="en"
 export LC_ALL="C"
 export ALLOW_MISSING_DEPENDENCIES=true
+
 export FOX_USE_NANO_EDITOR=1
 export FOX_ENABLE_APP_MANAGER=1
 export FOX_USE_BASH_SHELL=1
@@ -35,21 +37,20 @@ export FOX_USE_SED_BINARY=1
 export FOX_USE_XZ_UTILS=1
 export OF_ENABLE_FS_COMPRESSION="1"
 export FOX_DELETE_AROMAFM=1
+
 export FOX_REPLACE_BUSYBOX_PS=1
 export FOX_REPLACE_TOOLBOX_GETPROP=1
+
 export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	
+
+# Maintainer & Build Info
+export FOX_BUILD_TYPE="Testing"
+export FOX_MAINTAINER_PATCH_VERSION="0"
+export OF_MAINTAINER="Miranda's OFOX"
+
 # FRP_Test
 export OF_ENABLE_FRP_ADDON="1"
-
-# Settings
-export FOX_MAINTAINER_PATCH_VERSION="1"
-export OF_MAINTAINER="Miranda_OFRP"
-
- #version & variant
-export FOX_VARIANT="A16"
-export FOX_BUILD_TYPE="Testing"
 	
 	lunch twrp_$FDEVICE-eng
 	# let's see what are our build VARs
@@ -60,15 +61,3 @@ export FOX_BUILD_TYPE="Testing"
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
 fi
-
-
-# Modern Decryption Support (FBEv2 / Metadata)
-# Crucial for reading internal storage correctly on modern ROMs
-export OF_FBE_METADATA_MOUNT_IGNORE="1"
-	
-# Save OrangeFox settings in a safe path away from encryption complexities
-export FOX_USE_DATA_RECOVERY_FOR_SETTINGS="1" 
-
-# Screen Settings (To prevent GUI or touch glitches)
-export OF_SCREEN_H="2400"
-export OF_SCREEN_W="1080"
